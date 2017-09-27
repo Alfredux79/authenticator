88beed7f2934ead9d7a2047f4f5272fbecda21a5
alfredo-tappx <alfredo@tappx.com>
2017-09-27 22:42:07 +0200

    Initial commit

---
+++ src/main/java/io/alfredux/auth/Authenticator.java
@@ -0,0 +1,12 @@
+package io.alfredux.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Authenticator {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Authenticator.class, args);
+	}
+}
